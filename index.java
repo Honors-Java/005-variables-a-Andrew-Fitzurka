96dbec592c64df62b63760f596bc29cc8593e0ff
AndrewFitzurka1 <will_treaty@icloud.com>
2020-10-09 15:28:58 +0000

    Finished the assignment.  Messed with the x-value, y-value, & the diameter in draw()

--- index.java
+++ index.java
@@ -1,3 +1,10 @@
+int lowNum = 125;
+int highNum = 375;
+int diameter = 125;
+int xValue = random(15, 25);
+int yValue = random(15, 25);
+int numRandom = xValue + yValue;
+
 void setup() {
 	size(500, 500);
 }
@@ -14,12 +21,20 @@ void draw() {
 // change the value of the variables.
 // For example, “variable1 = variable1 + 2;”. 
 // Try different expressions and see what happens!
-
   background(255);
   stroke(0);
-  fill(175);
+  fill(31, 184, 72);
 
+// int highNum = 165;
+// int lowNum = 150;
+// int diameter = highNum - lowNum;
 
-  
- 
+//Top-left ellipse
+  ellipse(lowNum, lowNum, diameter, numRandom);
+//Top-right ellipse  
+  ellipse(highNum, lowNum, numRandom, diameter);
+//Bottom-left ellipse
+  ellipse(lowNum, highNum, numRandom, diameter);
+//Bottom-right ellipse
+  ellipse(highNum, highNum, diameter, numRandom);
 }
